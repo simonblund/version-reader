6e8afe4e2b14d7c675d42fffb1f3e13faf6ecf2a
Simon Blomsterlund <simon.blomsterlund@enplore.com>
2023-04-13 15:04:22 +0200

    new commit

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,10 +2,27 @@ import { describe, expect, test } from '@jest/globals';
 import {findVersionLine} from "."
 
 
-const exampleversion = '# This is a version file it is very complicated \n somethimes I like to make it many lines and forget to comment them out \n __version__="1.2.3"'
 describe("test parsing of versionstring", ()=>{
 
+
     test("find version", () =>{
-        expect(findVersionLine(exampleversion, '__version__="', '"')).toBe("1.2.3")
+        const exampleversion = '# This is a version file it is very complicated \n somethimes I like to make it many lines and forget to comment them out \n __version__="1.2.3"'
+        expect(findVersionLine(exampleversion, '__version__=')).toBe("1.2.3")
+    })
+    test("find version a lot of blanksteps", () =>{
+        const exampleversion = '# This is a version file it is very complicated \n somethimes I like to make it many lines and forget to comment them out \n __version__ = " 1.2.3 "'
+        expect(findVersionLine(exampleversion, '__version__=')).toBe("1.2.3")
+    })
+    test("find version single quotes", () =>{
+        const exampleversion = "# This is a version file it is very complicated \n somethimes I like to make it many lines and forget to comment them out \n __version__=' 1.2.3'"
+        expect(findVersionLine(exampleversion, '__version__=')).toBe("1.2.3")
+    })
+    test("find version", () =>{
+        const exampleversion = '# This is a version file it is very complicated \n somethimes I like to make it many lines and forget to comment them out \n __version__=" 1.2.3 "'
+        expect(findVersionLine(exampleversion, '__version__=')).toBe("1.2.3")
+    })
+    test("find version", () =>{
+        const exampleversion = '# This is a version file it is very complicated \n somethimes I like to make it many lines and forget to comment them out \n     __version__ = "   1.2.3   "'
+        expect(findVersionLine(exampleversion, '__version__=')).toBe("1.2.3")
     })
 })
--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ import * as fs from 'node:fs/promises';
 async function run(): Promise<void> {
     try {
         const prepend: string = core.getInput('prepend')
-        const append: string = core.getInput('append')
+        //const append: string = core.getInput('append')
         const filename: string = core.getInput('filename')
         const filelocation: string = core.getInput('filelocation')
         const allowFeatureBranches: boolean = core.getBooleanInput('allow_feature_branches')
 
         const fileContents = await readFile(filename, filelocation)
 
-        const versionString = findVersionLine(fileContents, prepend, append)
+        const versionString = findVersionLine(fileContents, prepend)
 
         if(versionString.includes("-") && !allowFeatureBranches){
             throw Error("This version file seems to contain a feature branch, Not allowed")
@@ -23,14 +23,23 @@ async function run(): Promise<void> {
     }
 }
 
-export function findVersionLine(content: string, prepend:string, append:string){
-    const contentLines = content.split("\n")
+export function findVersionLine(content: string, prepend:string){
+    const contentLines = content.replace(/ /g, "").split("\n")
+    console.log(contentLines)
     const versionline = contentLines.find(line => line.includes(prepend))
 
     if(versionline == undefined){
         throw Error("Could not find line that contains: " +prepend)
     }
-    const version = versionline.split(prepend)[1].split(append)[0]
+    const trimmed_verionline = versionline
+    .replace(" ", "")
+    .replace("\t", "")
+    .replace("\'", "")
+    .replace(/['"]+/g, '')
+    .replace("\\", "")
+
+    console.log(trimmed_verionline)
+    const version = trimmed_verionline.split(prepend)[1]
     return version
 
     
